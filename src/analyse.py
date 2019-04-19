cf537e21754a5ddb168385b84620c745fab08caf
elasticnet12345 <elasticnet1995@gmail.com>
2019-04-19 18:43:00 +0900

    [add] result

--- src/analyse.py
+++ src/analyse.py
@@ -10,9 +10,12 @@ import japanize_matplotlib
 
 
 
-
-
-
+def convert_minute(s):
+    """ 時間から分にする
+    """
+    s = s.replace("時間", ":").replace("分",":").split(":")
+    h, m = int(s[0]), int(s[1])
+    return h * 60 + m
 
 
 
@@ -28,7 +31,7 @@ def main():
     """ 
     memo = {}
     # names_ = ["Fuda-Shinjuku", "Fuda-Shinjuku-via-Chofu"]
-    names_ = [name for name in os.listdir("../data/") if "Ashigara" in name]
+    names_ = [name for name in os.listdir("../data/") if "Ashigara" in name and "Shinjuku" in name]
 
     for name in names_:
         path_dir = os.path.join("../data", name)
@@ -40,19 +43,29 @@ def main():
                 reader = csv.reader(f)
                 for minute, line in zip(range(0, 60), reader):
                     leaving_hour, leaving_minute = int(line[1][:2]), int(line[1][3:5])# 出発時刻(h)
-                    required_time = line[1][-3:-1]
+                    try:
+                        #required_time = int(line[1][-3:-1])
+                        _tmp = line[1][11:]
+                        required_time = convert_minute(_tmp)
+
+                    except:
+                        print("error:", line[1])
+                        print("error:", line[1][11:])
+                        exit()
+
                     # print(name, hour, minute, required_time)
                     if hour == leaving_hour:
                         required_time += leaving_minute - minute
                     else:
                         required_time += (60 + leaving_minute) - minute
-                    tmp.append(int(required_time))
+                    tmp.append(required_time)
         tmp = np.array(tmp)
         memo[name] = tmp
 
     colors_ = ["red", "green"]
-    labels_ = ["布田--新宿", "布田--調布--新宿"]
+    # labels_ = ["布田--新宿", "布田--調布--新宿"]
     # labels_ = ["足柄-新松田", "足柄-小田原-新松田"]
+    labels_ = ["足柄-新宿", "足柄-小田原-新宿"]
     for name, y in memo.items():
         x = np.arange(60*20)
         plt.plot(x, y, color=colors_[names_.index(name)], label=labels_[names_.index(name)])
@@ -64,8 +77,9 @@ def main():
     plt.xticks(locs, xtick, rotation="30")
     plt.xlabel("時刻")
     plt.ylabel("所要時間(分)")
-    plt.title("新宿駅までの所要時間")
+    # plt.title("新宿駅までの所要時間")
     # plt.title("新松田までの所要時間")
+    plt.title("新宿までの所要時間")
     plt.legend(loc="best")
     plt.show()
                     
